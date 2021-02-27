159d13ac00719b2ec2626f7386e92bf207e88ae7
cjglo <chris.j.gallo3@gmail.com>
2021-02-26 23:51:18 -0500

    Reading theory and doing basic set-up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
-fn main() {
-    
-    
+// Pong 
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+
+pub struct Pong;
+
+impl SimpleState for Pong { }
+
+fn main() -> Result<()> {
+
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataCuilder::default();
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    game.run();
+
+    // left off an "Creating an application"
+
+    // theory left off at "EntitiesRes"
+
+
+    Ok(())
 }
