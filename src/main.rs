84981ca7e5a7bba4ce047eeadfb6b9c9b02288ea
cjglo <chris.j.gallo3@gmail.com>
2021-03-11 23:24:11 -0500

    first part of tutorial finished! Rendering a black window lol

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,32 @@ pub struct Pong;
 
 impl SimpleState for Pong { }
 
-fn main() -> Result<()> {
+fn main() -> amethyst::Result<()> {
 
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
-    let game_data = GameDataCuilder::default();
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+            // RenderToWindow used to draw everything on window
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RednerFlat2D plugin is used to render entities with a 'SpriteRender'
+            .with_plugin(RenderFlat2D::default()),
+        )?;
+            
 
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
     game.run();
 
-    // left off an "Creating an application"
 
-    // theory left off at "EntitiesRes"
+    
 
 
     Ok(())
